6b647d9a48700c74474800f9a8cdf432292d18de
Pedro B <93920727+pedro-barreto@users.noreply.github.com>
2021-11-11 16:02:24 -0300

    slide temporário adicionado

---
+++ js/slide.js
@@ -0,0 +1,43 @@
+var intervalo = 3;
+
+function imagem1() {
+    
+    document.getElementById('imagem').src = 'img/1.jpg';
+    setTimeout('imagem2()',intervalo * 1000);
+
+}
+
+function imagem2() {
+    
+    document.getElementById('imagem').src = 'img/2.jpg';
+    setTimeout('imagem3()',intervalo * 1000);
+
+}
+
+function imagem3() {
+    
+    document.getElementById('imagem').src = 'img/3.jpg';
+    setTimeout('imagem4()',intervalo * 1000);
+
+}
+
+function imagem4() {
+    
+    document.getElementById('imagem').src = 'img/4.jpg';
+    setTimeout('imagem5()',intervalo * 1000);
+
+}
+
+function imagem5() {
+    
+    document.getElementById('imagem').src = 'img/5.jpg';
+    setTimeout('imagem6()',intervalo * 1000);
+
+}
+
+function imagem6() {
+    
+    document.getElementById('imagem').src = 'img/6.jpg';
+    setTimeout('imagem1()',intervalo * 1000);
+
+}
