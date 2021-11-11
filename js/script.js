8edbe33e72c2782429a82a5943120d5f79d91c66
Pedro B <93920727+pedro-barreto@users.noreply.github.com>
2021-11-11 12:13:33 -0300

    adicionado formulário

---
+++ js/script.js
@@ -0,0 +1,42 @@
+var nome = document.getElementById('cmpNome');
+var sobrenome = document.getElementById('cmpSobrenome');
+var telefone = document.getElementById('cmpTelefone');
+var email = document.getElementById('cmpEmail');
+var descricao = document.getElementById('cmpDescricao');
+var formulario = document.getElementsByTagName('form')[0];
+
+function enviar() {
+
+    const verificar = [...document.querySelectorAll('.campo')];
+
+    verificar.forEach(field =>{
+
+        if(field.value === ''){
+
+            formulario.classList.add('validar-campo');
+
+        }
+
+    });
+    
+    const formError = document.querySelector('.validar-campo');
+
+    if(formError){
+
+        formError.addEventListener('animationend',(event) =>{
+
+            if(event.animationName === 'campo-vazio'){
+
+                formError.classList.remove('validar-campo');
+
+            }
+
+        });
+
+    }else{
+
+        formulario.classList.add('form-hide');
+
+    }
+
+}
