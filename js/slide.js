b5f4a035e6933b205e0d8c9ff5ce92c9bbe87e3f
Pedro B <93920727+pedro-barreto@users.noreply.github.com>
2021-11-16 19:51:27 -0300

    adicionando fotos

--- js/slide.js
+++ js/slide.js
@@ -2,42 +2,21 @@ var intervalo = 3;
 
 function imagem1() {
     
-    document.getElementById('imagem-slide').src = 'img/1.png';
+    document.getElementById('imagem-slide').src = 'img/1.jpg';
     setTimeout('imagem2()',intervalo * 1000);
 
 }
 
 function imagem2() {
     
-    document.getElementById('imagem-slide').src = 'img/2.png';
+    document.getElementById('imagem-slide').src = 'img/2.jpg';
     setTimeout('imagem3()',intervalo * 1000);
 
 }
 
 function imagem3() {
     
-    document.getElementById('imagem-slide').src = 'img/3.png';
-    setTimeout('imagem4()',intervalo * 1000);
-
-}
-
-function imagem4() {
-    
-    document.getElementById('imagem-slide').src = 'img/4.png';
-    setTimeout('imagem5()',intervalo * 1000);
-
-}
-
-function imagem5() {
-    
-    document.getElementById('imagem-slide').src = 'img/5.png';
-    setTimeout('imagem6()',intervalo * 1000);
-
-}
-
-function imagem6() {
-    
-    document.getElementById('imagem-slide').src = 'img/6.png';
+    document.getElementById('imagem-slide').src = 'img/3.jpg';
     setTimeout('imagem1()',intervalo * 1000);
 
 }
