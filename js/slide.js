d75dd9a0a5f13883975a8b311c723545524156d3
Pedro B <93920727+pedro-barreto@users.noreply.github.com>
2021-11-16 17:01:05 -0300

    paleta de cores adicionada

--- js/slide.js
+++ js/slide.js
@@ -2,42 +2,42 @@ var intervalo = 3;
 
 function imagem1() {
     
-    document.getElementById('imagem-slide').src = 'img/1.jpg';
+    document.getElementById('imagem-slide').src = 'img/1.png';
     setTimeout('imagem2()',intervalo * 1000);
 
 }
 
 function imagem2() {
     
-    document.getElementById('imagem-slide').src = 'img/2.jpg';
+    document.getElementById('imagem-slide').src = 'img/2.png';
     setTimeout('imagem3()',intervalo * 1000);
 
 }
 
 function imagem3() {
     
-    document.getElementById('imagem-slide').src = 'img/3.jpg';
+    document.getElementById('imagem-slide').src = 'img/3.png';
     setTimeout('imagem4()',intervalo * 1000);
 
 }
 
 function imagem4() {
     
-    document.getElementById('imagem-slide').src = 'img/4.jpg';
+    document.getElementById('imagem-slide').src = 'img/4.png';
     setTimeout('imagem5()',intervalo * 1000);
 
 }
 
 function imagem5() {
     
-    document.getElementById('imagem-slide').src = 'img/5.jpg';
+    document.getElementById('imagem-slide').src = 'img/5.png';
     setTimeout('imagem6()',intervalo * 1000);
 
 }
 
 function imagem6() {
     
-    document.getElementById('imagem-slide').src = 'img/6.jpg';
+    document.getElementById('imagem-slide').src = 'img/6.png';
     setTimeout('imagem1()',intervalo * 1000);
 
 }
