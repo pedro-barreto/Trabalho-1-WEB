14e70c2febcf61dc38c498f2586d56fe2b5a9019
Pedro B <93920727+pedro-barreto@users.noreply.github.com>
2021-11-16 23:35:12 -0300

    organização

--- js/slide.js
+++ js/slide.js
@@ -2,21 +2,21 @@ var intervalo = 3;
 
 function imagem1() {
     
-    document.getElementById('imagem-slide').src = 'img/1.jpg';
+    document.getElementById('imagem-slide').src = 'img/slide/1.jpg';
     setTimeout('imagem2()',intervalo * 1000);
 
 }
 
 function imagem2() {
     
-    document.getElementById('imagem-slide').src = 'img/2.jpg';
+    document.getElementById('imagem-slide').src = 'img/slide/2.jpg';
     setTimeout('imagem3()',intervalo * 1000);
 
 }
 
 function imagem3() {
     
-    document.getElementById('imagem-slide').src = 'img/3.jpg';
+    document.getElementById('imagem-slide').src = 'img/slide/3.jpg';
     setTimeout('imagem1()',intervalo * 1000);
 
 }
